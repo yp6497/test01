6f9e45af38c5f30ff226afdbfea4d5e6a7b8d689
yp6497 <yp6497@bs.amalnet.k12.il>
2020-01-20 20:12:45 +0200

    updated file

--- app/src/main/java/com/example/test01/MainActivity.java
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int y=0;
+        int z=0;
     }
 
 }
