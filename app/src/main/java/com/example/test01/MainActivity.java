371fdeac670e057190de742a34f561c84913f343
yp6497 <yp6497@bs.amalnet.k12.il>
2020-02-10 16:40:06 +0200

    updated file

--- app/src/main/java/com/example/test01/MainActivity.java
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-
+    TextView tv;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        btn=(Button)findViewById(R.id.btn);
         setContentView(R.layout.activity_main);
+
 
     }
 
     public void go(View view) {
+        String str="oh yea,i have been clicked";
+        tv.setText(str);
     }
 }
